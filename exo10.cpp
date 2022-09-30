760cfaa419ce6904577c818d5a1219ababd4a03f
HugoBunelier <bunelierpro@gmail.com>
2022-09-30 19:17:42 +0200

    fix 10

--- exo10.cpp
+++ exo10.cpp
@@ -1,25 +1,53 @@
- /**************************************************************************
- *                                                                         *
- *                                                                         *
- *      by: BUNELIER Hugo                    ::::::::     ::::::::         *
- *                                          :+:    :+:   :+:    :+:        *
- *                                                +:+   +:+    +:+         *
- *      github: bubudotsh                     +#++:     +#++:++#           *
- *                                              +#+   +#+    +#+           *
- *      Created: 2022-09-30 11:05:46     #+#   #+#   #+#    #+#            *
- *      Updated: 2022-09-30 11:05:49     ########    ########              *
- *                                                                         *
- *                                                                         *
- ***************************************************************************/
+/**********************************************
+
+   Description: 
+
+   by: BUNELIER Hugo | github bubudotsh
+
+   Created: 2022-09-30 18:45:43
+   Updated: 2022-09-30 19:02:28
+ 
+************************************************/
 
 #include <iostream>
-#include <math.h>
+#include <numeric>
+#include <vector>
 
 using namespace std;
 
 
 int main()
 {
+    int max, x;
+    vector<double> tab {};
+    cout << "entrer vos nombre" << endl;
+
+    while (true) {
+        double nb;
+        cin >> nb;
+
+        if (nb == -1) {
+            break;
+        }
+
+        tab.push_back(nb);
+    }
+
+    // compter le nombre de vleur dans le tableau
+
+    for (int i=0;i<50;++i) {
+
+        if(tab[i]>max) {
+            max=tab[i];
+        }
+
+    }
+
+
+    double moyenne = (accumulate(begin(tab), end(tab), 0)) / tab.size();
+
+    cout << "moyenne: " << moyenne << "; max: " << max << endl;
+
 
     cin.get();
     cin.ignore();
